78bc6226e41be2d1d54043bbafec88a9cd7f7eec
Mayank Ranjan <Mayank.Ranjan@philips.com>
2020-09-10 09:19:43 +0530

    Add Sender_Visit and Receiver_Visit

---
+++ Sender_Visit/Sender_Visit/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Data;
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String file = System.Configuration.ConfigurationManager.AppSettings["filename"];
+            String path = Directory.GetCurrentDirectory();
+            path += @"\" + file;
+            FileReader fileReader = new FileReader();
+
+            DataTable dt = null;
+            if (fileReader.CheckFileExists(path))
+            {
+                dt = CSVToDatatable.ConvertCSVtoDataTable(path);
+                int Columns = dt.Columns.Count;
+                Console.WriteLine(Columns);
+                foreach (var columns in dt.Columns)
+                {
+                    Console.WriteLine(columns.ToString());
+                }
+                int Rows = dt.Rows.Count;
+                Console.WriteLine(Rows);
+                for (int i = 0; i < Rows; i++)
+                {
+                    for (int j = 0; j < Columns; j++)
+                    {
+                        Console.Write(dt.Rows[i][j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
