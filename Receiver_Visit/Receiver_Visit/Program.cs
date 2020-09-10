953e7bc84ec9ea23d909f66b6647b06b7c1e85b2
Tanmoy Debnath <35931043+debnathtanmoy@users.noreply.github.com>
2020-09-10 21:01:10 +0530

    Update Program.cs

--- Receiver_Visit/Receiver_Visit/Program.cs
+++ Receiver_Visit/Receiver_Visit/Program.cs
@@ -1,4 +1,32 @@
-﻿using System;
+// ﻿using System;
+// using System.Data;
+
+
+// namespace Receiver_Visit
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             DataTable dt = new DataTable();
+//             ReadConsole datatablereader = new ReadConsole();
+//             dt = datatablereader.AddColumns(dt);
+//             dt = datatablereader.AddRows(dt);
+
+//             for (int i = 0; i < dt.Rows.Count; i++)
+//             {
+                
+//                 for (int j = 0; j < dt.Columns.Count; j++)
+//                 {
+//                     Console.Write(dt.Rows[i][j] + " ");
+//                 }
+//                 Console.WriteLine();
+//             }
+//         }
+//     }
+// }
+
+using System;
 using System.Data;
 
 
@@ -9,19 +37,35 @@ namespace Receiver_Visit
         static void Main(string[] args)
         {
             DataTable dt = new DataTable();
-            ReadConsole datatablereader = new ReadConsole();
-            dt = datatablereader.AddColumns(dt);
-            dt = datatablereader.AddRows(dt);
+            dt.Columns.Add("Time", typeof(DateTime));
+            dt.Columns.Add("source");
+            dt.Rows.Add("2020-01-10 07:12:06.163");
+            dt.Rows.Add("2020-2-10 09:10:06.163");
+            dt.Rows.Add("2020-3-10 09:15:06.163");
+            dt.Rows.Add("2020-4-10 05:14:06.163");
+            dt.Rows.Add("2020-5-10 07:20:06.163");
+            dt.Rows.Add("2020-6-10 08:20:06.163");
+            dt.Rows.Add("2020-11-10 08:20:06.163");
 
-            for (int i = 0; i < dt.Rows.Count; i++)
+            foreach (DataRow dataRow in dt.Rows)
             {
-                
-                for (int j = 0; j < dt.Columns.Count; j++)
+                foreach (var item in dataRow.ItemArray)
                 {
-                    Console.Write(dt.Rows[i][j] + " ");
+                    Console.WriteLine(item);
                 }
-                Console.WriteLine();
             }
+
+            Analytics an = new Analytics();
+            an.AverageInHour(dt);
+            an.AvergaeInweek(dt);
+            an.PeakLastMonth(dt);
+
+
         }
     }
+
 }
+
+
+
+
