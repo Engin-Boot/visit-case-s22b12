d7501c7cce6eb9ecedf329df4b35fd25bb6e2f0e
Tanmoy Debnath <35931043+debnathtanmoy@users.noreply.github.com>
2020-09-12 12:47:45 +0530

    Update Program.cs

--- Receiver_Visit/Receiver_Visit/Program.cs
+++ Receiver_Visit/Receiver_Visit/Program.cs
@@ -1,31 +1,3 @@
-// ﻿using System;
-// using System.Data;
-
-
-// namespace Receiver_Visit
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             DataTable dt = new DataTable();
-//             ReadConsole datatablereader = new ReadConsole();
-//             dt = datatablereader.AddColumns(dt);
-//             dt = datatablereader.AddRows(dt);
-
-//             for (int i = 0; i < dt.Rows.Count; i++)
-//             {
-                
-//                 for (int j = 0; j < dt.Columns.Count; j++)
-//                 {
-//                     Console.Write(dt.Rows[i][j] + " ");
-//                 }
-//                 Console.WriteLine();
-//             }
-//         }
-//     }
-// }
-
 using System;
 using System.Data;
 using System.Globalization;
@@ -82,11 +54,13 @@ namespace Receiver_Visit
             if(dt!=null)
             {
                 Analytics an = new Analytics();
-                double avghour=an.AverageInHour(dt,date);
-                double avgweek=an.AvergaeInweek(dt, date);
-                int avgpeak=an.PeakLastMonth(dt);
+                double avghour=Analytics.AverageInHour(dt,date);
+                double avgweek= Analytics.AvergaeInweek(dt, date);
+                int avgpeak= Analytics.PeakLastMonth(dt);
+                Console.WriteLine(avghour);
+                Console.WriteLine(avgweek);
+                Console.WriteLine(avgpeak);
 
-               
                 DataTable dtresults = new DataTable();
 
                 String storingresultsfile = "Results.csv";
