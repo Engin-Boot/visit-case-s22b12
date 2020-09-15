d5352a9b5ebe520f781572b436659bffd0cbb427
Tanmoy Debnath <35931043+debnathtanmoy@users.noreply.github.com>
2020-09-15 12:11:52 +0530

    Update DataTableTOCSV.cs

--- Receiver_Visit/Receiver_Visit/DataTableTOCSV.cs
+++ Receiver_Visit/Receiver_Visit/DataTableTOCSV.cs
@@ -8,23 +8,40 @@ namespace Receiver_Visit
 {
     public static class DataTableTocsv
     {
-       
-            public static void ToCsv(DataTable dt, string strFilePath)
-            {
-            StringBuilder sb = new StringBuilder();
 
-            IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
-                                              Select(column => column.ColumnName);
-            sb.AppendLine(string.Join(",", columnNames));
-
-            foreach (DataRow row in dt.Rows)
+        public static bool ToCsv(DataTable dt, string strFilePath)
+        {
+            try
             {
-                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
-                sb.AppendLine(string.Join(",", fields));
+                StringBuilder sb = new StringBuilder();
+
+
+
+                IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().Select(column => column.ColumnName);
+                sb.AppendLine(string.Join(",", columnNames));
+
+
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                    sb.AppendLine(string.Join(",", fields));
+                }
+
+
+
+                File.WriteAllText(strFilePath, sb.ToString());
+                return true;
             }
 
-            File.WriteAllText(strFilePath, sb.ToString());
+
+
+            catch
+            {
+                return false;
+            }
         }
-        }
-    
+
+    }
+
 }
